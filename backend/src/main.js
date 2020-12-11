480526e0ef02a65159e669edcc4134baca1edceb
ToolReaz <toolreaz@outlook.fr>
2020-12-11 20:02:31 +0100

    Add middlewares

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,11 +1,24 @@
-const { default: fastify } = require("fastify");
+const { fastify } = require("fastify");
 
+// Load env config
 require("dotenv").config();
+
+// Register middlewares
+fastify.register(require("fastify-compress"));
+fastify.register(require("fastify-cors"), {
+  origin: "*",
+});
+fastify.register(require("fastify-cookie"), {
+  secret: "my-secret",
+});
+fastify.register(require("fastify-helmet"), {
+  hidePoweredBy: { setTo: "Potatoes" },
+});
 
 const server = require("fastify")({ logger: true });
 
-server.get('/', (req, res) => {
-    res.send("Hello World !")
+server.get("/", (req, res) => {
+  res.send("Hello World !");
 });
 
 server.listen(process.env.PORT || 3000, (err, address) => {

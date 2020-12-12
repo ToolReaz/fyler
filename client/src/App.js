5fe8c035012aafe622a795c16f38d19987117914
ToolReaz <toolreaz@outlook.fr>
2020-12-12 13:37:49 +0100

    Client setup and link creation

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Menu, Breadcrumb } from "antd";
+import { Link, Route, Switch } from "react-router-dom";
+import HomePage from "./pages/homePage";
+import LoginPage from "./pages/loginPage";
+import RegisterPage from "./pages/registerPage";
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout style={{ minHeight: "100vh" }}>
+      <Header theme="light">
+        <div id="logo">
+          <Link to="/">
+            <h1 style={{ color: "#FFF" }}>Fyler</h1>
+          </Link>
+        </div>
+        <Menu
+          style={{ float: "right" }}
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={[0]}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Menu.Item key="1">
+            <Link to="/register">S'inscrire</Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link to="/login">Me connecter</Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: 50 }}>
+        <Switch>
+          <Route path="/" exact={true}>
+            <HomePage />
+          </Route>
+          <Route path="/register">
+            <RegisterPage />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+        </Switch>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        Flyer &nbsp;-&nbsp; GPL v3 Licence &nbsp;-&nbsp; by ToolReaz
+      </Footer>
+    </Layout>
   );
 }
 
---
+++ client/src/pages/homePage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CreateLink from "../components/CreateLink";
+
+export default function HomePage() {
+  return (
+    <div style={{ margin: "0 auto" }}>
+      <CreateLink />
+    </div>
+  );
+}
